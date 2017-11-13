9145dbc3dd6cdde625e0ccaec440eaccb2314df0
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-11-13 14:15:51 -0300

    uploading multiple times insstead of all at once

--- js/main.js
+++ js/main.js
@@ -12,43 +12,51 @@ document.addEventListener(
       form.dispatchEvent(new CustomEvent('submit'));
     });
     form.addEventListener('submit', function (e) {
+      e.preventDefault();
       var files = [].slice.call(input.files, 0);
-      if (files.length < 1) return e.preventDefault();
-      var form = new FormData();
-      files.forEach(function (file) {
+      var index = 0;
+      var length = files.length;
+      if (length < 1) return;
+      input.disabled = true;
+      upload.disabled = true;
+      (function upload() {
+        var form = new FormData();
+        var file = files.shift();
+        index++;
         form.append(input.name, file);
-      });
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', '/upload', true);
-      xhr.upload.onprogress = function (e) {
-        if (e.lengthComputable) {
-          progress.max = e.total;
-          progress.value = e.loaded;
-        }
-      };
-      xhr.onabort = xhr.onerror = xhr.onload = function (e) {
-        input.value = '';
-        progress.max = 1;
-        progress.value = 0;
-        input.disabled = false;
-        upload.disabled = false;
-        if (e.type === 'error') {
-          alert('Something wrong: ' + (e.message || 'unknown'));
-        } else {
-          files.forEach(function (file) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/upload', true);
+        xhr.upload.onprogress = function (e) {
+          if (e.lengthComputable) {
+            var max = length * e.total;
+            var current = index * e.total;
+            var value = current - e.total + e.loaded;
+            progress.max = max;
+            progress.value = value;
+          }
+        };
+        xhr.onabort = xhr.onerror = xhr.onload = function (e) {
+          if (!files.length || e.type === 'error') {
+            input.value = '';
+            progress.max = 1;
+            progress.value = 0;
+            input.disabled = false;
+            upload.disabled = false;
+          }
+          if (e.type === 'error') {
+            alert('Something wrong: ' + (e.message || 'unknown'));
+          } else {
             if (Trashbin.files.indexOf(file.name) < 0) {
               Trashbin.files.unshift(file.name);
             }
-          });
-          list.innerHTML = Trashbin.files.map(function (name) {
-            return '<li><a href="/' + name + '" download>' + name + '</a></li>';
-          }).join('');
-        }
-      };
-      xhr.send(form);
-      input.disabled = true;
-      upload.disabled = true;
-      e.preventDefault();
+            list.innerHTML = Trashbin.files.map(function (name) {
+              return '<li><a href="/' + name + '" download>' + name + '</a></li>';
+            }).join('');
+            if (files.length) upload();
+          }
+        };
+        xhr.send(form);
+      }());
     }, false);
   },
   {once: true}
